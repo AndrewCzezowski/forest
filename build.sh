652eba41b92815c17ec14c32780e421559d7c870
aczezowski <andrew.m.czezowski@gmail.com>
2026-04-08 16:39:38 -0500

    Fixing build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-git worktree remove output/forest
+git worktree remove output/forest --force
 git worktree add output/forest gh-pages --no-checkout
 cp output/forest/.git ~.git.tmp
 forester build forest.toml
