e1f8bccc7484232241f3f25dce092bd10e6ac40e
aczezowski <andrew.m.czezowski@gmail.com>
2026-04-17 18:33:46 -0500

    Changes to Pre-render XML files to html

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ git worktree remove output/forest --force
 git worktree add output/forest gh-pages --no-checkout
 cp output/forest/.git ~.git.tmp
 forester build forest.toml
+bash expand.sh
 mv ~.git.tmp output/forest/.git
 
 echo 'Commit and push changes to gh-pages to update.'
---
+++ expand.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Generate output/FOO/index.html for each output/FOO/index.xml
+
+set -e
+XSL_FILE="output/forest/default.xsl"
+
+for xml in output/forest/*/index.xml; do
+  [ -f "$xml" ] || continue
+  html="${xml%index.xml}index.html"
+  echo "Generating $html ..."
+  xsltproc "$XSL_FILE" "$xml" > "$html" &
+done
+
+cp output/forest/index/index.html output/forest/index.html
+
+wait
+echo "✅ All index.html files generated under output/forest."
